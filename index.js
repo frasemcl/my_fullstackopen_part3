834982a8e586817a9477916dc26d74bc704a7e19
Fraser McLaughlin <fraser.mclaughlin@outlook.com>
2022-12-22 14:00:02 -0500

    done ex 3.21

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
 
-// const generateId = () => Math.floor(Math.random() * 10000);
-
 ////////////////////////
 // App route handlers //
 /////////////////////////
@@ -64,28 +62,31 @@ app.put('/api/persons/:id', (request, response, next) => {
 		number: body.number,
 	};
 
-	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+	Person.findByIdAndUpdate(request.params.id, person, {
+		new: true,
+		runValidators: true,
+		context: 'query',
+	})
 		.then(updatedPerson => {
 			response.json(updatedPerson);
 		})
 		.catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const body = request.body;
-
-	if (!body.name || !body.number) {
-		return response.status(400).json({ error: 'content missing' });
-	}
 
 	const person = new Person({
 		name: body.name,
 		number: body.number,
 	});
 
-	person.save().then(savedPerson => {
-		response.json(savedPerson);
-	});
+	person
+		.save()
+		.then(savedPerson => {
+			response.json(savedPerson);
+		})
+		.catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -108,6 +109,8 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' });
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message });
 	}
 
 	next(error);
